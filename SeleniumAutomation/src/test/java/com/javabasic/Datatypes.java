ad3f505aaa43a878290b7ed34dd637e85b825721
Temonidas <69393817+Temonidas@users.noreply.github.com>
2020-08-18 20:33:00 -0500

    Segundo Commit

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,37 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	      /*  byte x;
+	        short y;
+	        int z;
+	        long a;
+	       
+	        x=127;
+	        y=32767;
+	        z=2147483647;
+	        a=9223372036854775807L;
+	       
+	        System.out.println(a);
+
+	*/
+		
+		char letra1;
+		char letra2;
+		String frase;
+		
+		letra1= 'a';
+		letra2= 'x';
+		frase="Hola";
+		
+		System.out.println(frase);
+
+		//boolean avesVuelan;
+	//	avesvuelan
+	        
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasic/OperatorsStatements.java
@@ -0,0 +1,36 @@
+package com.javabasic;
+
+public class OperatorsStatements {
+	
+	public static void main(String[] args) {
+		
+//		int x=1;
+//		int y=2;
+//		int suma;
+//		
+//		suma=x+y;
+//		
+//		System.out.println(suma);
+
+		
+//		int valor1 =3;
+//		int incrementar = ++valor1;
+//		
+//		System.out.println(incrementar);
+//		
+		int num1=3;
+		int num2=4;
+		int num3 =6;
+		int num4=11;
+		
+		if(num1==num2) {
+		System.out.println("Los numeros son iguales");
+		}else {
+			System.out.println("Los numeros son iguales");
+		}
+		
+		
+		
+		
+}
+}
